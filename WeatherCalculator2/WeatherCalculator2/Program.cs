6a7ba2dd9b81f9776b6c4d1e009c4acb57964730
levi-kicken <59981699+levi-kicken@users.noreply.github.com>
2020-01-30 16:27:26 -0600

    Update Program.cs

--- WeatherCalculator2/WeatherCalculator2/Program.cs
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -6,12 +6,15 @@ namespace WeatherCalculator2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("### Weather Calculator 2");
+            Console.WriteLine("*** Weather Calculator 2 ***");
 
             int temperature;
             int windSpeed;
+            int humid;
+            double heatIndex;
             double windChill;
             string input = "";
+            string input2 = "";
 
             // Get the temperature from the user
             Console.WriteLine("Enter the temperature");
@@ -20,18 +23,34 @@ namespace WeatherCalculator2
 
             temperature = int.Parse(input);
 
-            // Get the windspeed from the user
-            Console.WriteLine("Enter the wind speed");
+            if (temperature < 50)
+            {
+                // Get the windspeed from the user
+                Console.WriteLine("Enter the wind speed");
 
-            input = Console.ReadLine();
+                input = Console.ReadLine();
 
-            windSpeed = int.Parse(input);
+                windSpeed = int.Parse(input);
 
-            // Calculate the wind chill
+                // Calculate the wind chill
 
-            windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+                windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
 
-            Console.WriteLine("The wind chill is " + windChill);
+                Console.WriteLine("The wind chill is " + windChill);
+            }
+
+            else
+            {
+                Console.WriteLine("Enter the relative humidity");
+
+                input2 = Console.ReadLine();
+
+                humid = int.Parse(input2);
+
+                heatIndex = 0.5 * (temperature + 61.0 + (temperature - 68.0) * 1.2) + humid * 0.094;
+
+                Console.WriteLine("The heat index is " + heatIndex);
+            }
         }
     }
 }
